1b5e5c5e8714ac7f60ed19067e8138264414299b
Elsenhuber <elsenhuc@amazon.de>
2018-11-14 10:14:42 +0100

    updated index.php

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>AWSOME Demo</title>
+        <title>AWSOME Demo for Deployment</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>DEMO Deployment for AWS</h1>
                 <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS</p>
+                <p>Your PHP application is now running on a container in Amazon ECS - Very Cool</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
